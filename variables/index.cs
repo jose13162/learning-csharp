80b176160ce74c2e2371b5e3eacdf6ca7b35c6a5
jose13162 <anselmojose297@gmail.com>
2022-09-07 20:53:54 -0300

    Types and variables

---
+++ variables/index.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Variables
+{
+  static void Main()
+  {
+    int firstNumber = 5, secondNumber = 7, result;
+
+    result = firstNumber * secondNumber;
+
+    Console.WriteLine("Result: " + result);
+  }
+}
