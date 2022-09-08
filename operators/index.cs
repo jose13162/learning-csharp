121fec7c6457ebe5f9cbdebb20c038a3f4379c10
jose13162 <anselmojose297@gmail.com>
2022-09-08 20:55:15 -0300

    Operators

---
+++ operators/index.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Operators {
+  static void Main() {
+    bool boolean1 = false & false; // false
+    bool boolean2 = true & false; // false
+    bool boolean3 = true & true; // true
+    bool boolean3 = false | false; // false
+    bool boolean3 = true | false; // true
+    bool boolean3 = true | true; // true
+  }
+}
