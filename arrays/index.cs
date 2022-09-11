693d1309b6bb1835b2d9ce9d70769dc939701b04
jose13162 <anselmojose297@gmail.com>
2022-09-10 21:25:29 -0300

    Arrays

---
+++ arrays/index.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Arrays {
+  static void Main(string[] args) {
+    int[] primeNumbers = new int[6];
+    primeNumbers[0] = 2;
+    primeNumbers[1] = 3;
+    primeNumbers[2] = 5;
+    primeNumbers[3] = 7;
+    primeNumbers[4] = 11;
+    primeNumbers[5] = 13;
+
+    Console.WriteLine(primeNumbers[0]);
+    Console.WriteLine(primeNumbers[1]);
+    Console.WriteLine(primeNumbers[2]);
+    Console.WriteLine(primeNumbers[3]);
+    Console.WriteLine(primeNumbers[4]);
+    Console.WriteLine(primeNumbers[5]);
+  }
+}
