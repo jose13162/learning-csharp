ee993f24e3f776ecee5dcd968cfa68a80525ca5d
jose13162 <anselmojose297@gmail.com>
2022-09-11 15:04:43 -0300

    Update If conditional

--- if-conditional/index.cs
+++ if-conditional/index.cs
@@ -2,23 +2,11 @@ using System;
 
 class IfConditional {
   static void Main() {
-    string result = "Reprovado";
+    bool isInvalid = false;
 
-    Console.Write("Informe sua nota 1: ");
-    int score1 = int.Parse(Console.ReadLine());
-    Console.Write("Informe sua nota 2: ");
-    int score2 = int.Parse(Console.ReadLine());
-    Console.Write("Informe sua nota 3: ");
-    int score3 = int.Parse(Console.ReadLine());
-    Console.Write("Informe sua nota 4: ");
-    int score4 = int.Parse(Console.ReadLine());
-
-    int average = (score1 + score2 + score3 + score4) / 4;
-
-    if (average >= 60) {
-      result = "Aprovado";
+    if (!isInvalid) {
+      Console.WriteLine("It is valid");
+      Console.WriteLine(!isInvalid);
     }
-
-    Console.WriteLine("Média: {0}\nResultado: {1}", average, result);
   }
 }
